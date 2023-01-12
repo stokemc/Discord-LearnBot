f053244ab36867591e84af1a2d585ac53b792887
stokemc <stoke@stoketech.club>
2023-01-12 19:36:54 +0000

    Update bot.py

---
+++ bot.py
@@ -0,0 +1,24 @@
+# website: http://stoketech.club - email: stoke@stoketech.club
+
+# IMPORT OUR NEEDED MODULES...
+import discord
+import course_anaconda
+from private.config import token
+
+
+# SET OUR VARIABLES FOR EASIER DISPLAY IN BOT...
+outline1 = (course_anaconda.course +'\n\n' + course_anaconda.outline_1)
+outline2 = (course_anaconda.course +'\n\n' + course_anaconda.outline_2)
+outline3 = (course_anaconda.course +'\n\n' + course_anaconda.outline_3)
+outline4 = (course_anaconda.course +'\n\n' + course_anaconda.outline_4)
+outline5 = (course_anaconda.course +'\n\n' + course_anaconda.outline_5)
+outline6 = (course_anaconda.course +'\n\n' + course_anaconda.outline_6)
+outline7 = (course_anaconda.course +'\n\n' + course_anaconda.outline_7)
+lesson1 = (course_anaconda.course +'\n\n' + course_anaconda.lesson_1)
+lesson2 = (course_anaconda.course +'\n\n' + course_anaconda.lesson_2)
+lesson3 = (course_anaconda.course +'\n\n' + course_anaconda.lesson_3)
+lesson4 = (course_anaconda.course +'\n\n' + course_anaconda.lesson_4)
+lesson5 = (course_anaconda.course +'\n\n' + course_anaconda.lesson_5)
+lesson6 = (course_anaconda.course +'\n\n' + course_anaconda.lesson_6)
+lesson7 = (course_anaconda.course +'\n\n' + course_anaconda.lesson_7)
+
