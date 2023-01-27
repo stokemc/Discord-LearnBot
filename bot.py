597cbd3b336b3a90247c66cadf85f77fd2226d5b
stokemc <stoke@stoketech.club>
2023-01-27 08:45:36 +0000

    Sorted out the token variable now stored in /private/config.py Updated to fresh environment in Anaconda

--- bot.py
+++ bot.py
@@ -1,10 +1,9 @@
 # website: http://stoketech.club - email: stoke@stoketech.club
-
-# IMPORT OUR NEEDED MODULES...
 import discord
 import course_anaconda
 from private.config import token
 
+print("...imports successful...")
 
 # SET OUR VARIABLES FOR EASIER DISPLAY IN BOT...
 outline1 = (course_anaconda.course +'\n\n' + course_anaconda.outline_1)
@@ -14,11 +13,12 @@ outline4 = (course_anaconda.course +'\n\n' + course_anaconda.outline_4)
 outline5 = (course_anaconda.course +'\n\n' + course_anaconda.outline_5)
 outline6 = (course_anaconda.course +'\n\n' + course_anaconda.outline_6)
 outline7 = (course_anaconda.course +'\n\n' + course_anaconda.outline_7)
-lesson1 = (course_anaconda.course +'\n\n' + course_anaconda.lesson_1)
-lesson2 = (course_anaconda.course +'\n\n' + course_anaconda.lesson_2)
-lesson3 = (course_anaconda.course +'\n\n' + course_anaconda.lesson_3)
-lesson4 = (course_anaconda.course +'\n\n' + course_anaconda.lesson_4)
-lesson5 = (course_anaconda.course +'\n\n' + course_anaconda.lesson_5)
-lesson6 = (course_anaconda.course +'\n\n' + course_anaconda.lesson_6)
-lesson7 = (course_anaconda.course +'\n\n' + course_anaconda.lesson_7)
+lesson1 = (course_anaconda.course +'\n' + course_anaconda.lesson_1)
+lesson2 = (course_anaconda.course +'\n' + course_anaconda.lesson_2)
+lesson3 = (course_anaconda.course +'\n' + course_anaconda.lesson_3)
+lesson4 = (course_anaconda.course +'\n' + course_anaconda.lesson_4)
+lesson5 = (course_anaconda.course +'\n' + course_anaconda.lesson_5)
+lesson6 = (course_anaconda.course +'\n' + course_anaconda.lesson_6)
+lesson7 = (course_anaconda.course +'\n' + course_anaconda.lesson_7)
 
+print(lesson1)
